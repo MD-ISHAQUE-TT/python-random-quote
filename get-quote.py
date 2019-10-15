687739b2555c85e7fd710da3862bb5b2b81c11e7
MD-ISHAQUE-TT <md.ishaque.tecnotree@gmail.com>
2019-10-16 02:58:11 +0530

    Issue 4 tasks like file writing operation is done

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,56 @@
+import sys
 import random
 def primary():
   #print("Keep it logically awesome.")
+  #f = open("quotes.txt",'w')
+##  with open("qts.txt",'r+b') as f:
+##    qts = f.readlines()
+##    f.seek(0)
+##    for qt in qts:
+##      qt = f.read()
+##      print(qt)
+##      #qt='"'+qt+'"'
+##      f.write(qt)   
+##    #f.close()
   
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-  last = len(quotes)-1
-  rnd = random.randint(0,last)
+  a = input('Enter your Quote: "')
+  a = '"'+a+'"'
+  print(type(a))
+  b = input('Do You Want To add This Quote to your List ?')
+  
+  if b == 'Y' or b == 'y':
+    with open("quotes.txt",'a+') as f:
+      with open("qts.txt", 'a+') as f2:
+        #f.write("\n")
+        f.write("\n"+a)
+##        f2.write("\n"+a)
+        f.close()
+##        f2.close()
+        
+  with open("quotes.txt",'r') as f:
+    quotes = f.readlines()
+    f.close()
+    
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.write(
+  #f.close()
+    
+  last = len(quotes)
+  #rnd = random.randint(0,last)
   #print(quotes[-1])
   #print(quotes[len(quotes)-1])
-  print(quotes[rnd])
+  #print(quotes[rnd])
+  
+  for i in range(last):
+    #sys.stdout.write("QUOTE {}: {}".format(i+1,quotes[i]))# On Same line to print
+    #sys.stdout.flush()                                    # Same use and used along with above line
+    #print("QUOTE {}: {}".format(i+1,quotes[i]),end=" ",flush=True)
+    print("QUOTE ",(i+1),": ",quotes[i],end='')
+#    print()
+##  print("\n======")
+##  for i in range(len(qts)):
+##    print("QUOTE ",(i+1),": ",qts[i],end='')
   
 
 if __name__== "__main__":
